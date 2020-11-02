c94bc3927ba575a00339563bef83e5ddc86193b0
tom <tomhall4321@gmail.com>
2020-11-02 14:14:14 +0000

    elegantFizzBuzz

--- 01-day/01-basic-php/07-fizzbuzz.php
+++ 01-day/01-basic-php/07-fizzbuzz.php
@@ -2,14 +2,31 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
+// for ($i = 1; $i <= 100; $i += 1){
+//     if ($i % 3 === 0 && $i % 5 === 0){
+//         dump("FizzBuzz");
+//     } else if ($i % 3 === 0) {
+//         dump("Fizz");
+//     } else if ($i % 5 === 0) {
+//         dump("Buzz");
+//     } else {
+//         dump($i);
+//     }
+// }
+
 for ($i = 1; $i <= 100; $i += 1){
-    if ($i % 3 === 0 && $i % 5 === 0){
-        dump("FizzBuzz");
-    } else if ($i % 3 === 0) {
-        dump("Fizz");
-    } else if ($i % 5 === 0) {
-        dump("Buzz");
-    } else {
-        dump($i);
+    $fizzBuzzOutput = "";
+
+    if($i % 3 === 0){
+        $fizzBuzzOutput .= "Fizz";
     }
-}
+    if($i % 5 === 0){
+        $fizzBuzzOutput .= "Buzz";
+    }
+    if($i % 2 === 0){
+        $fizzBuzzOutput .= "Jazz";
+    }
+
+    dump($fizzBuzzOutput !== "" ? $fizzBuzzOutput : $i);
+
+    }
