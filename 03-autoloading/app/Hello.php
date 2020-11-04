d99b1d2c1f269249448cec3ece3be1e711086dcf
tom <tomhall4321@gmail.com>
2020-11-04 12:33:48 +0000

    namespace structure

---
+++ 03-autoloading/app/Hello.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+
+class Hello
+{
+    public function hello($name)
+    {
+        return "Hello {$name}";
+    }
+}
---
+++ 03-autoloading/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+echo "\nQuestion 1:\n";
+
+use App\Hello;
+
+$sayHi = new Hello();
+
+dump($sayHi->hello("Orb")); // "Hello Orb"
+dump($sayHi->hello("Horse")); // "Hello Horse"
