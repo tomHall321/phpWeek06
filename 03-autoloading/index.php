0b509ab8910aea1b83762d7371c0ddf5376f6d0d
tom <tomhall4321@gmail.com>
2020-11-04 14:18:14 +0000

    BasketItem

--- 03-autoloading/index.php
+++ 03-autoloading/index.php
@@ -45,3 +45,18 @@ dump($book->currentPage()); // 13 - start on page 1
 // read another 25 pages
 $book->read(25);
 dump($book->currentPage()); // 38
+
+echo "\nQuestion 4:\n";
+
+use App\Shopping\BasketItem;
+
+$item = new BasketItem("coffee", 3.50);
+
+// get the type
+dump($item->type()); // "coffee"
+
+// get the price as a number
+dump($item->price()); // 3.5
+
+// get the price as a formatted string
+dump($item->priceFormatted()); // "£3.50"
